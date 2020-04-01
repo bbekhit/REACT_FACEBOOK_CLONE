c8f087d00828d91280748a6428595d29779c8c4a
Boutros Bekhit <bbekhit@apple.com>
2020-04-01 11:14:09 -0700

    prepare deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 dotenv.config();
 
 // db;
@@ -31,6 +32,16 @@ app.use(cors());
 app.use("/api/v1/auth", require("./routes/auth"));
 app.use("/api/v1/post", require("./routes/post"));
 app.use("/api/v1/profile", require("./routes/profile"));
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5001;
 
